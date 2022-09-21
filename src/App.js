71f647254c1bd6921adc7bbcfbd0bd159eb28577
ifeoma-imoh <imoh_ifeoma@yahoo.co.uk>
2022-09-21 13:25:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
-function App() {
+let animationController;
+
+export default function App() {
+  const [file, setFile] = useState(null);
+  const canvasRef = useRef();
+  const audioRef = useRef();
+  const source = useRef();
+  const analyzer = useRef();
+
+  const handleAudioPlay = () => {
+    let audioContext = new AudioContext();
+    if (!source.current) {
+      source.current = audioContext.createMediaElementSource(audioRef.current);
+      analyzer.current = audioContext.createAnalyser();
+      source.current.connect(analyzer.current);
+      analyzer.current.connect(audioContext.destination);
+    }
+    visualizeData();
+  };
+
+  const visualizeData = () => {
+    animationController = window.requestAnimationFrame(visualizeData);
+    if (audioRef.current.paused) {
+      return cancelAnimationFrame(animationController);
+    }
+    const songData = new Uint8Array(140);
+    analyzer.current.getByteFrequencyData(songData);
+    const bar_width = 3;
+    let start = 0;
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    for (let i = 0; i < songData.length; i++) {
+      // compute x coordinate where we would draw a
+      start = i * 4;
+      //create a gradient for the  whole canvas
+      let gradient = ctx.createLinearGradient(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      gradient.addColorStop(0.2, "#2392f5");
+      gradient.addColorStop(0.5, "#fe0095");
+      gradient.addColorStop(1.0, "purple");
+      ctx.fillStyle = gradient;
+      ctx.fillRect(start, canvasRef.current.height, bar_width, -songData[i]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        onChange={({ target: { files } }) => files[0] && setFile(files[0])}
+      />
+      {file && (
+        <audio
+          ref={audioRef}
+          onPlay={handleAudioPlay}
+          src={window.URL.createObjectURL(file)}
+          controls
+        />
+      )}
+      <canvas ref={canvasRef} width={500} height={200} />
     </div>
   );
 }
-
-export default App;
